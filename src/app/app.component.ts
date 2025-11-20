636ca987d53771ab33be21345c20667722a07db0
user <user@DESKTOP-33U8FNM>
2025-11-20 08:52:56 +0530

    changesgit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'otpread';
+  mainObj:any={};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  myOTP:any;
+  ngAfterViewInit() {
+    if ('OTPCredential' in window) {
+        this.mainObj.isWebOtpSupported = true;
+        debugger;
+        window.addEventListener('DOMContentLoaded', e => {
+            debugger;
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest('form');
+        debugger;
+        if (form) {
+            debugger;
+            form.addEventListener('submit', e => {
+            ac.abort();
+            });
+        }
+        var reqObj =  {
+          otp: { transport:['sms'] },
+          signal: ac.signal
+      };
+        navigator.credentials.get(
+          reqObj
+        ).then((otp:any) => {
+            debugger;
+            if(
+              otp
+            ){
+              if(
+                otp && otp.code
+              ){
+                // alert('GOT OTP***'+ otp.code);
+                // input.value = otp.code;
+                this.myOTP = otp.code;
+              }
+            }
+            
+            // if (form) form.submit();
+        }).catch(err => {
+            debugger;
+            console.log(err);
+        });
+        });
+    }else{
+      // this.myOTP = 521456;
+      this.mainObj.isWebOtpSupported = false;
+      // alert('Web OTP API not supported, Please enter manually.');
+    }
+  }
 }
